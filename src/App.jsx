d5d490eadd590f2aca95d1fdbcaa9e10271073e8
ManojTella <manojtella7@gmail.com>
2024-07-09 15:19:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; // Assuming you have a separate CSS file for styling
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <h2>Name: Manoj Tella</h2>
+        <p>Registration No: 212221240026</p>
+        <button>Show Details</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
